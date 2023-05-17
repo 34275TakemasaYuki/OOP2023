4c13644ee14bf1f9757fd4f628350ad599718bc4
34275TakemasaYuki <OIBC34275@ojs.ac.jp>
2023-05-17 11:21:39 +0900

    P79問題2.1完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Song {
+
+        // 2.1.1 プロパティ
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        // 2.1.2 コンストラクタ
+        public Song(string title, string artistname, int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+    }
+}
