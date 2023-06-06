4131f1c4d1fef7ecffa7a517af8c0ca29bfc6d79
34275TakemasaYuki <OIBC34275@ojs.ac.jp>
2023-06-06 09:23:30 +0900

    演習問題5.4完成

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -8,6 +8,11 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var words = line.Split('=', ';');
+
+            Console.WriteLine("作家　：" + words[1]);
+            Console.WriteLine("代表作：" + words[3]);
+            Console.WriteLine("誕生年：" + words[5]);
         }
     }
 }
