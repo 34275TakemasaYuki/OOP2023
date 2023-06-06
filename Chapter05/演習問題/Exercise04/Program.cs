498a5da049467ab2384c636b0829f115bcd874fd
34275TakemasaYuki <OIBC34275@ojs.ac.jp>
2023-06-06 11:14:10 +0900

    演習問題5.4タイマー追加

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,8 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 #if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
             var words = line.Split('=', ';');
@@ -20,11 +22,22 @@ namespace Exercise04 {
             Console.WriteLine("誕生年：" + words[5]);
  
 #elif StringArray
+
+
             string[] lines = {
                 "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
                 "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
                 "Novelist=太宰治;BestWork=人間失格;Born=1909",
                 "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+               
             };
 
             foreach (var word in lines)
@@ -36,6 +49,7 @@ namespace Exercise04 {
                 Console.WriteLine("誕生年：" + words[5]);
             }
 #endif
+            Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\.fffff"));//時間表示
         }
     }
 }
