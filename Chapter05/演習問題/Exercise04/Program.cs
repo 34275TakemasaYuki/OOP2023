3ccb4c18672086bd81bca1f0ba5e27b81ab09692
34275TakemasaYuki <OIBC34275@ojs.ac.jp>
2023-06-06 09:45:37 +0900

    演習問題5.4配列版完成

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿#define StringArray
+//#define NonArray
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +10,32 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+
+#if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
             var words = line.Split('=', ';');
 
             Console.WriteLine("作家　：" + words[1]);
             Console.WriteLine("代表作：" + words[3]);
             Console.WriteLine("誕生年：" + words[5]);
+ 
+#elif StringArray
+            string[] lines = {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            };
+
+            foreach (var word in lines)
+            {
+                var words = word.Split('=', ';');
+
+                Console.WriteLine("作家　：" + words[1]);
+                Console.WriteLine("代表作：" + words[3]);
+                Console.WriteLine("誕生年：" + words[5]);
+            }
+#endif
         }
     }
 }
