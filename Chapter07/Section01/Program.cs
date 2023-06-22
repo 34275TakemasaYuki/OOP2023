a233c3311d720926298e854e221d1221a8ea863d
34275TakemasaYuki <OIBC34275@ojs.ac.jp>
2023-06-22 11:12:33 +0900

    県庁所在地確認プログラム完成

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            string city;
+            string capital;
+            string cityCheck;
+            int flag = 0;
+            var cityDict = new Dictionary<string, string>();
+
+            Console.WriteLine("県庁所在地の登録");
+            while (true)
+            {
+                Console.Write("県名:");
+                city = Console.ReadLine();
+                if (city == "999")
+                    break;
+
+                Console.Write("県庁所在地:");
+                capital = Console.ReadLine();
+                if (!cityDict.ContainsKey(city) || !cityDict.ContainsKey(capital))
+                {
+                    cityDict[city] = capital;
+                }
+                else
+                {
+                    Console.WriteLine("この情報はすでに登録されています。");
+                }
+                flag = 1;
+            }
+
+            if (flag == 1)
+            {
+                Console.Write("確認する県名を入力:");
+                cityCheck = Console.ReadLine();
+                Console.WriteLine("{0}です。", cityDict[cityCheck]);
+            }
+        }
+    }
+}
