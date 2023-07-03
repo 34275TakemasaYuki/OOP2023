d41014127509dd7cdf7ea63bf79fd4a779fbf1b1
34275TakemasaYuki <OIBC34275@ojs.ac.jp>
2023-07-03 14:03:37 +0900

    演習問題8.3完成

---
+++ Chapter08/演習問題/Exercise03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var tw = new TimeWatch();
+            var startTime = tw.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("test");
+            }
+            TimeSpan duration = tw.Stop(startTime);
+            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+        }
+    }
+}
---
+++ Chapter08/演習問題/Exercise03/TimeWatch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class TimeWatch {
+        public DateTime Start() {
+            return DateTime.Now;
+        }
+
+        public TimeSpan Stop(DateTime startTime) {
+            return DateTime.Now - startTime;
+        }
+    }
+}
