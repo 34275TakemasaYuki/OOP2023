e4d0356d8f1e14d65cabff51806838bec9a2bb37
竹政　優希 <OIBC34275@ojs.ac.jp>
2023-09-13 09:25:14 +0900

    インサート完了

---
+++ Chapter13/SampleEntityFramework/Program.cs
@@ -0,0 +1,48 @@
+﻿using SampleEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntityFramework {
+    class Program {
+        static void Main(string[] args) {
+            InsertBooks();
+            Console.Write("データを挿入しました。続けるにはEnterキーを押してください。");
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+        // List 13-5
+        static void InsertBooks() {
+            using (var db = new BooksDbContext())
+            {
+                var book1 = new Book
+                {
+                    Title = "坊ちゃん",
+                    PublishedYear = 2003,
+                    Author = new Author
+                    {
+                        Birthday = new DateTime(1867, 2, 9),
+                        Gender = "M",
+                        Name = "夏目漱石",
+                    }
+                };
+                db.Books.Add(book1);
+                var book2 = new Book
+                {
+                    Title = "人間失格",
+                    PublishedYear = 1990,
+                    Author = new Author
+                    {
+                        Birthday = new DateTime(1909, 6, 19),
+                        Gender = "M",
+                        Name = "太宰治",
+                    }
+                };
+                db.Books.Add(book2);
+                db.SaveChanges();
+            }
+        }
+    }
+}
