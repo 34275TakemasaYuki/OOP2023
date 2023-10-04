6e8120d6746e6544825393f43324c165b57dd881
竹政　優希 <OIBC34275@ojs.ac.jp>
2023-10-04 09:33:14 +0900

    1004ブラウザ閲覧アプリ改善途中経過

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -29,6 +29,10 @@ namespace RssReader {
             this.lbRssTitle = new System.Windows.Forms.ListBox();
             this.wbBrowser = new System.Windows.Forms.WebBrowser();
             this.tbInfo = new System.Windows.Forms.TextBox();
+            this.gbGenre = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.gbGenre.SuspendLayout();
             this.SuspendLayout();
             // 
             // tbUrl
@@ -53,9 +57,9 @@ namespace RssReader {
             // 
             this.lbRssTitle.FormattingEnabled = true;
             this.lbRssTitle.ItemHeight = 12;
-            this.lbRssTitle.Location = new System.Drawing.Point(12, 65);
+            this.lbRssTitle.Location = new System.Drawing.Point(12, 281);
             this.lbRssTitle.Name = "lbRssTitle";
-            this.lbRssTitle.Size = new System.Drawing.Size(368, 592);
+            this.lbRssTitle.Size = new System.Drawing.Size(368, 376);
             this.lbRssTitle.TabIndex = 2;
             this.lbRssTitle.Click += new System.EventHandler(this.lbRssTitle_Click);
             // 
@@ -76,11 +80,46 @@ namespace RssReader {
             this.tbInfo.Size = new System.Drawing.Size(444, 31);
             this.tbInfo.TabIndex = 4;
             // 
+            // gbGenre
+            // 
+            this.gbGenre.Controls.Add(this.radioButton2);
+            this.gbGenre.Controls.Add(this.radioButton1);
+            this.gbGenre.Location = new System.Drawing.Point(12, 65);
+            this.gbGenre.Name = "gbGenre";
+            this.gbGenre.Size = new System.Drawing.Size(368, 194);
+            this.gbGenre.TabIndex = 5;
+            this.gbGenre.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.radioButton1.Location = new System.Drawing.Point(18, 31);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 28);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "IT";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.radioButton2.Location = new System.Drawing.Point(18, 65);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(98, 28);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "エンタメ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1272, 691);
+            this.Controls.Add(this.gbGenre);
             this.Controls.Add(this.tbInfo);
             this.Controls.Add(this.wbBrowser);
             this.Controls.Add(this.lbRssTitle);
@@ -89,6 +128,8 @@ namespace RssReader {
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbGenre.ResumeLayout(false);
+            this.gbGenre.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +142,9 @@ namespace RssReader {
         private System.Windows.Forms.ListBox lbRssTitle;
         private System.Windows.Forms.WebBrowser wbBrowser;
         private System.Windows.Forms.TextBox tbInfo;
+        private System.Windows.Forms.GroupBox gbGenre;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
