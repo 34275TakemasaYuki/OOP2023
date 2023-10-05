d40f8440d79db3d962cec46abb6850730137ca1d
竹政　優希 <OIBC34275@ojs.ac.jp>
2023-10-05 10:40:40 +0900

    Chapter10のExercise3完成

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+
+            for (int i = 0; i < texts.Length; i++)
+            {
+                var matches = Regex.Matches(texts[i], @"\b[T|t]ime\b");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine("{0} : {1}",texts[i],match.Index);
+                }
+            }
+        }
+    }
+}
