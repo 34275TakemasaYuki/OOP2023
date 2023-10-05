e131979c015d44e24a68ef7259b7a59a05c1f6eb
竹政　優希 <OIBC34275@ojs.ac.jp>
2023-10-05 11:26:44 +0900

    Chapter10のExercise4完成

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
+            var pattern = @"[Vv]ersion\s*=\s*""v4.0""";
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = Regex.Replace(lines[i],pattern ,"version=\"v5.0\"");
+            }
 
-
-
+            File.WriteAllLines("sample.txt", lines);
 
 
 
