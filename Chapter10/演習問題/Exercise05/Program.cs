c3fa39f6885229e037ba50a5152ff62079da7762
竹政　優希 <OIBC34275@ojs.ac.jp>
2023-10-10 11:24:54 +0900

    Chapter10のExercise5模範解答

--- Chapter10/演習問題/Exercise05/Program.cs
+++ Chapter10/演習問題/Exercise05/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise05 {
@@ -14,7 +15,15 @@ namespace Exercise05 {
         private static void TagLower(string file) {
             var lines = File.ReadLines(file);
             var sb = new StringBuilder();
-
+            foreach (var line in lines)
+            {
+                var s = Regex.Replace(line,@"<(/?)([A-Z][A-Z0-9]*)(.*)",
+                    m =>
+                    {
+                        return string.Format("<{0} {1} {2}>", m.Groups[1].Value, m.Groups[2].Value.ToLower(), m.Groups[3].Value);
+                    });
+                sb.AppendLine(s);
+            }
 
 
 
@@ -23,6 +32,10 @@ namespace Exercise05 {
 
             //ファイル出力
             File.WriteAllText(file, sb.ToString());
+
+            // これ以降は確認用
+            var text = File.ReadAllText("sample.html");
+            Console.WriteLine(text);
         }
     }
 }
