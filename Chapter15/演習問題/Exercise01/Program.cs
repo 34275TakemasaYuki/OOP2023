e097ccfc93422e7ad5a47712d15a269ccceec3f1
竹政　優希 <OIBC34275@ojs.ac.jp>
2023-10-11 11:58:20 +0900

    Chapter15のExercise1-2完成

---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var maxPrice = Library.Books.Max(b => b.Price);
+            var books = Library.Books.Where(b => b.Price == maxPrice);
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+
+        private static void Exercise1_3() {
+            
+        }
+
+        private static void Exercise1_4() {
+            
+        }
+
+        private static void Exercise1_5() {
+            
+        }
+
+        private static void Exercise1_6() {
+            
+        }
+
+        private static void Exercise1_7() {
+            
+        }
+
+        private static void Exercise1_8() {
+            
+        }
+    }
+}
