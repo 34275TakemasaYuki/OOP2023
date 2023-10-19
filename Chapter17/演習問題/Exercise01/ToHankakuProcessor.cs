c7393e62d7da1792d0c80d643c191ddd94c33c94
竹政　優希 <OIBC34275@ojs.ac.jp>
2023-10-19 12:03:59 +0900

    演習問題17-2まで

---
+++ Chapter17/演習問題/Exercise01/ToHankakuProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class ToHankakuProcessor :TextProcessor{
+
+        private static new Dictionary<char, char> __dictionary =
+            new Dictionary<char, char>()
+            {
+                {'０','0'},{'１','1'},{'２','2'},{'３','3'},{'４','4'},
+                {'５','5'},{'６','6'},{'７','7'},{'８','8'},{'９','9'},
+            };
+
+        protected override void Execute(string line) {
+            var s = Regex.Replace(line,"[０-９]", c => __dictionary[c.Value[0]].ToString());
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Chapter17/演習問題/Exercise02/ConcreteConverter/KmeterConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.ConcreteConverter {
+    public class KmeterConverter : Framework.ConverterBase{
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "kmeter"|| name == UnitName;
+        }
+
+        protected override double Ratio { get { return 1000; } }
+        public override string UnitName { get { return "キロメートル"; } }
+    }
+}
